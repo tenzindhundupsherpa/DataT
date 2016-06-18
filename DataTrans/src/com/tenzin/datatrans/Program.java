401ada29ee5e245ca7069c1e475c0794bc2af14d
tenzindhundupsherpa <tenzinnono6@gmail.com>
2016-06-18 22:02:04 +0545

    io

--- DataTrans/src/com/tenzin/datatrans/Program.java
+++ DataTrans/src/com/tenzin/datatrans/Program.java
@@ -6,6 +6,11 @@
 package com.tenzin.datatrans;
 
 import com.tenzin.datatrans.controller.Controller;
+import java.io.File;
+import java.io.IOException;
+import java.io.FileWriter;
+import java.io.FileReader;
+
 
 
 /**
@@ -17,7 +22,27 @@ public class Program {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) {        
+    // try{
+      
+         
+         
+         /*FileWriter writer=new FileWriter("D:\\DataT\\DataTrans\\src\\com\\tenzin\\datatrans\\rawdata\\data\\data.txt");
+       writer.write("1,Ram,Nepali,ram@gmail.com,1\r\n");
+        writer.write("2,tashi,Gurung,tashi@gmail.com,0\r\n");
+         writer.write("3,karsang,tamang,karsang@gmail.com,1\r\n");
+         writer.write("4,hari,Nepali,hari@gmail.com,0\r\n");
+         writer.write("5,dawa,lama,lamagmail.com,1\r\n");
+       writer.close();*/
+     //}catch(IOException ioa){
+        // System.out.println(ioa.getMessage());
+     
+         
+        
+        
+        
+        
+        
         Controller controller = new Controller();
         
         controller.add();
--- DataTrans/src/com/tenzin/datatrans/controller/Controller.java
+++ DataTrans/src/com/tenzin/datatrans/controller/Controller.java
@@ -8,55 +8,63 @@ package com.tenzin.datatrans.controller;
 import com.tenzin.datatrans.dao.StudentDAO;
 import com.tenzin.datatrans.dao.StudentDAOImpl;
 import com.tenzin.datatrans.entity.Student;
-import com.tenzin.datatrans.rawdata.RawData;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 /**
  *
  * @author Home
  */
 public class Controller {
-     StudentDAO studentDAO=new StudentDAOImpl();
-    
-    
-    public void add(){
-          String[] rd=RawData.data();
-       
-       for(int i=0;i<5;i++){
-           String[] tokens=rd[i].split(",");
-       
-       if(tokens.length<=5){
-           Student std=new Student();
-           std.setId(Integer.parseInt(tokens[0]));
-           std.setfName(tokens[1]);
-           std.setlName(tokens[2]);
-           std.setEmail(tokens[3]);
-           if(Integer.parseInt(tokens[4])==0){
-               std.setStatus(false);
-           }else if(Integer.parseInt(tokens[4])==1){
-               std.setStatus(true);
-           }
-           boolean a = studentDAO.checkEmail(std);
-           boolean b = studentDAO.checkId(std);
-          
-           studentDAO.add(std);
-       
-       }
+
+    StudentDAO studentDAO = new StudentDAOImpl();
+
+    public void add() {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("D:\\DataT\\DataTrans\\src\\com\\tenzin\\datatrans\\rawdata\\data\\data.txt"));
+            String l = "";
+            while ((l = reader.readLine()) != null) {
+
+                
+                    String[] tokens = l.split(",");
+
+                    if (tokens.length <= 5) {
+                        Student std = new Student();
+                        std.setId(Integer.parseInt(tokens[0]));
+                        std.setfName(tokens[1]);
+                        std.setlName(tokens[2]);
+                        std.setEmail(tokens[3]);
+                        if (Integer.parseInt(tokens[4]) == 0) {
+                            std.setStatus(false);
+                        } else if (Integer.parseInt(tokens[4]) == 1) {
+                            std.setStatus(true);
+                        }
+                        boolean a = studentDAO.checkEmail(std);
+                        boolean b = studentDAO.checkId(std);
+
+                        studentDAO.add(std);
+
+                    
+                }
+            }
+        } catch (IOException ioe) {
+            System.out.println(ioe.getMessage());
+        }
     }
-    
+
+    public void showAll() {
+        for (Student sd : studentDAO.showAll()) {
+            System.out.println("ID:" + sd.getId());
+            System.out.println("First Name:" + sd.getfName());
+            System.out.println("Last Name:" + sd.getlName());
+            System.out.println("Email:" + sd.getEmail());
+            if (sd.isStatus() == true) {
+                System.out.println("Status: valid");
+            } else if (sd.isStatus() != true) {
+                System.out.println("Status: invalid");
+            }
+        }
     }
-    
-    public void showAll(){
-        for(Student sd:studentDAO.showAll()){
-         System.out.println("ID:" + sd.getId());
-         System.out.println("First Name:" + sd.getfName());
-         System.out.println("Last Name:" + sd.getlName());
-         System.out.println("Email:" + sd.getEmail());
-         if(sd.isStatus()==true){
-             System.out.println("Status: valid");   
-         }else if(sd.isStatus()!=true){
-             System.out.println("Status: invalid");
-         }
-     }  
-    }
-    
+
 }
